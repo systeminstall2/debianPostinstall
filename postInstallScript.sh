136c865dbce10b794dd96bfd55883b59e797bccf
systeminstall2 <156584743+systeminstall2@users.noreply.github.com>
2024-05-27 14:45:27 +0200

    Update postInstallScript.sh

--- postInstallScript.sh
+++ postInstallScript.sh
@@ -45,7 +45,7 @@ wget https://portswigger.net/burp/releases/community/latest
 VERSION=$(cat latest | grep -E "....\..\..\.." --color=auto -o | head -n 1)
 wget "https://portswigger-cdn.net/burp/releases/download?product=community&version=2024.3.1.4&type=Linux" -O burp.sh
 chmod +x burp.sh
-printf "o\n \ny\n \n" | ./burp.sh -c
+./burp.sh -c
 #sudo mv ~/BurpSuiteCommunity/ /opt/Burpsuite
 #echo "bash /opt/Burpsuite/BurpSuiteCommunity" | sudo tee /usr/bin/Burpsuite
 #sudo chmod +x /usr/bin/Burpsuite
