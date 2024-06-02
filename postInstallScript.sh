1b02b7be5be7e6df72b88096c8c4dff65b652092
systeminstall2 <156584743+systeminstall2@users.noreply.github.com>
2024-06-02 02:15:18 +0200

    Update postInstallScript.sh

--- postInstallScript.sh
+++ postInstallScript.sh
@@ -41,7 +41,7 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
-sudo apt-get update -y
+echo "a" | sudo -S apt-get update -y
 sudo apt-get install code -y
 sudo apt-get autoremove -y
 
