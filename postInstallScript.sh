63c473215fdd8349aa9027d5652d0ed9563f7367
systeminstall2 <156584743+systeminstall2@users.noreply.github.com>
2024-06-05 15:23:43 +0200

    Update postInstallScript.sh

--- postInstallScript.sh
+++ postInstallScript.sh
@@ -127,6 +127,8 @@ export NVM_DIR="$HOME/.nvm"
 nvm install --lts
 echo "export PATH=/home/n0ne/.local/bin:\$PATH" >> ~/.zshenv
 
+/bin/bash -c 'chmod +x /home/n0ne/debianPostinstall/installLvim.sh && bash /home/n0ne/debianPostinstall/installLvim.sh && bash /home/n0ne/runAfterReboot.sh && echo "a" | sudo -S locale-gen de_DE.UTF-8'
+
 #finishing
 
 sudo rm -rf /etc/sudoers.d/custom_sudoers
