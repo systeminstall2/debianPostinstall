359e65663d24abaff3b04f657a9f9d2d8f0719ea
systeminstall2 <156584743+systeminstall2@users.noreply.github.com>
2024-06-05 17:41:35 +0200

    Update installLvim.sh

--- installLvim.sh
+++ installLvim.sh
@@ -2,6 +2,6 @@ echo "a" | sudo -S chmod 777 /opt/
 
 LV_BRANCH='release-1.3/neovim-0.9'
 rustup default stable && curl -s "https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh" > installLunarvim.sh && chmod +x installLunarvim.sh && printf "y\nn\ny" | ./installLunarvim.sh
-rm -rf ~/.config/lvim && cp lvim ~/.config/lvim -r
+rm -rf ~/.config/lvim && cp ~/debianPostinstall/lvim ~/.config/ -r
 
 sudo chmod 755 /opt/
